807845b916044f8d180a386bbc98d376f9702512
Desmond Chen <desmondddd@gmail.com>
2018-11-16 10:15:08 +0800

    improved store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,8 +6,9 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     lastFailedUpload: [],
-    jwt_token: '',
+    jwt_token: localStorage.getItem('jwt_token') || '',
   },
+
   getters: {
     getLastFailedUpload(state) {
       for (let upload of state.lastFailedUpload) {
@@ -15,20 +16,39 @@ export default new Vuex.Store({
       }
       return state.lastFailedUpload;
     },
+
     getJwtToken(state) {
-      return state.jwt_token;
+      if (state.jwt_token) {
+        let base64Url = state.jwt_token.split('.')[1];
+        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        let exp_date = new Date(JSON.parse(window.atob(base64)).exp * 1000);
+        //如果过期
+        if (exp_date < new Date()) {
+          state.jwt_token = '';
+          localStorage.setItem('jwt_token', '');
+        }
+
+        return state.jwt_token;
+      }
+      return '';
     },
   },
+
   mutations: {
     setLastFailedUpload(state, newFailedUpload) {
       state.lastFailedUpload = newFailedUpload;
     },
+
     setJwtToken(state, new_token) {
       state.jwt_token = new_token;
+      localStorage.setItem('jwt_token', new_token);
     },
+
     resetJwtToken(state) {
       state.jwt_token = '';
+      localStorage.setItem('jwt_token', '');
     },
   },
+
   actions: {},
 });
