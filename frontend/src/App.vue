565098f157bab19c6db461b3c717fbfb1d16d052
Desmond Chen <desmondddd@gmail.com>
2019-02-03 17:40:56 +0800

    format changed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,30 @@
 <template>
   <div id="app">
     <AppHeader></AppHeader>
-    <router-view />
+    <router-view/>
     <AppFooter></AppFooter>
   </div>
-
 </template>
 
 <script>
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 // import axios from 'axios';
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 export default {
   components: {
     AppHeader: Header,
-    AppFooter: Footer,
+    AppFooter: Footer
   },
   methods: {
-    ...mapActions(['init_jwt_token', 'logout']),
+    ...mapActions(["init_jwt_token", "logout"])
   },
   created() {
     this.init_jwt_token();
   },
   destroyed() {
     this.logout();
-  },
+  }
 };
 </script>
 
@@ -35,7 +34,8 @@ export default {
  No media query for `xs` since this is the default in Bootstrap */
 /*  Small devices (landscape phones, 576px and up) */
 #app {
-  font-family: 'Helvetica Neue', Helvetica, Arial, 'STXihei', 'Hiragino Sans GB', 'Microsoft YaHei', sans-serif;
+  font-family: "Helvetica Neue", Helvetica, Arial, "STXihei", "Hiragino Sans GB",
+    "Microsoft YaHei", sans-serif;
 }
 
 @media (min-width: 576px) {
